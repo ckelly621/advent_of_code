58325a7147229eba70ef70a8177edb9b145b6752
Charlie <61924651+ckelly621@users.noreply.github.com>
2021-12-25 01:19:20 -0500

    Solves day 6 (#20)

---
+++ 2021_advent_of_code/day06/src/test/scala/com/ckelly/day6/LanternFishSolverTest.scala
@@ -0,0 +1,18 @@
+package com.ckelly.day6
+
+import com.ckelly.day6.LanternFishSolver.solvePartOne
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class LanternFishSolverTest extends Specification {
+  "Day06" should {
+    "should return 5 for part one" in {
+      solvePartOne("/example.txt") mustEqual 5934L
+    }
+    "should return 12 for part two" in {
+      solvePartOne("/example.txt", 256) mustEqual 26984457539L
+    }
+  }
+}
